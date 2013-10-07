764754c8535c800a31a98dc1aa0a046a1c2d9fd1
syll0012 <syll0012@umn.edu>
2013-10-07 15:21:35 -0500

    wd5

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -1,11 +1,28 @@
 ﻿Option Strict On
 Public Class Form1
 
-
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
 
-        txtHyp.Text = "Not Implemented Yet"
+        Dim x As Double = CDbl(txtSideOne.Text)
+        Dim y As Double = CDbl(txtSideTwo.Text)
+        Dim z As Double
+
+        z = Hypotenuse(x, y)
+
+        txtHyp.Text = CStr(z)
 
     End Sub
 
+
+    Function Hypotenuse(ByVal a As Double, ByVal b As Double) As Double
+
+        Dim c As Double
+
+        c = Math.Sqrt(a ^ 2 + b ^ 2)
+
+        Return c
+
+        'the three above lines couuld have been written Return.Math.Sqrt(a ^ 2 + b ^ 2)
+
+    End Function
 End Class
